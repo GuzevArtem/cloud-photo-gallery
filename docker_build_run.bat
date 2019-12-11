56d7d26ee949a02257d23984a6eac0ed377ad49d
GuzevArtem\Beshinei <guzev.artem@gmail.com>
2019-12-11 02:10:56 +0200

    Fixed docker_build_run.bat and remove obsolete requirements

--- docker_build_run.bat
+++ docker_build_run.bat
@@ -1,4 +1,4 @@
-docker build --rm -f "Dockerfile" -t cpd:latest .
+docker build -f "Dockerfile" -t cpd:latest ./
 docker run -p 5000:5000 -p 80:80/tcp -p 80:80/udp -p 430:430/tcp -d --name master cpd:latest
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' master
 PAUSE
